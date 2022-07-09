1c59c1fba94577d3a984a631174a0d201dbcbb7d
DaeYoung <geungan9@gmail.com>
2022-07-09 09:34:00 +0900

    feat: add hashfactory test

--- src/test/java/HashFactory/HashTest.java
+++ src/test/java/HashFactory/HashTest.java
@@ -15,74 +15,55 @@ class HashTest {
     }
 
     @Test
+    //private getHashCode 테스트
     void  testGetHashCode()
         throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Hash hash = new Hash(10);
         //given
+        Hash hash = new Hash(10);
         Method method = Hash.class.getDeclaredMethod("getHashCode", String.class);
         method.setAccessible(true);
-        int expectedInt = 414;
 
         //when
-        int actual = (int) method.invoke(hash, "asdf");
+        int result = (int) method.invoke(hash, "asdf");
+        int actual = 414;
         //then
-        assertEquals(expectedInt, actual);
+        assertEquals(result, actual);
 
     }
 
     @Test
+    //private getindex테스트
     void  testGetIndex()
         throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Hash hash = new Hash(10);
         //given
+        Hash hash = new Hash(10);
         Method method = Hash.class.getDeclaredMethod("getIndex", int.class);
         method.setAccessible(true);
-        int expectedInt = 4;
 
         //when
-        int actual = (int) method.invoke(hash, 414);
+        int actual = 4;
+        int result = (int) method.invoke(hash, 414);
         //then
-        assertEquals(expectedInt, actual);
+        assertEquals(result, actual);
 
     }
 
-    @Test
-    void  testGetIndex2()
-        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Hash hash = new Hash(10);
-        //given
-        Method method = Hash.class.getDeclaredMethod("getIndex", int.class);
-        method.setAccessible(true);
-        int expectedInt = 0;
-
-        //when
-        int actual = (int) method.invoke(hash, 0);
-        //then
-        assertEquals(expectedInt, actual);
-
-    }
-
-    @Test
-    //잠시 보류
-    void  testSearchNode()
-        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
-        Hash hash = new Hash(10);
-        Field field = hash.getClass().getDeclaredField("table");
-        field.setAccessible(true);
-
-
-        //given
-        Method method = Hash.class.getDeclaredMethod("getIndex", int.class);
-        method.setAccessible(true);
-        int expectedInt = 4;
-
-        //when
-        int actual = (int) method.invoke(hash, 414);
-        //then
-        assertEquals(expectedInt, actual);
-
-    }
-
+//    @Test
+//    void  testGetIndex2()
+//        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//        //given
+//        Hash hash = new Hash(10);
+//        Method method = Hash.class.getDeclaredMethod("getIndex", int.class);
+//        method.setAccessible(true);
+//        int actual = 0;
+//
+//        //when
+//        int actual = 0;
+//        int result = (int) method.invoke(hash, 0);
+//        //then
+//        assertEquals(result, actual);
+//
+//    }
 
 
     @Test
@@ -95,11 +76,10 @@ class HashTest {
         //then
         assertEquals(result, "aa");
 
-
-
     }
 
     @Test
+    //해시에 값이 없을 때,
     void  testGet2() {
         //given
         Hash hash = new Hash(10);
@@ -113,7 +93,7 @@ class HashTest {
     }
 
     @Test
-    //같은 해시코드 = 같은 인덱스에 우치할 때,
+    //값을 다르지만, 같은 배열 인덱스에 존재할 때,
     void  testGet3() {
         //given
         Hash hash = new Hash(10);
@@ -129,7 +109,7 @@ class HashTest {
     }
 
     @Test
-    //같은 키값이 와서 밸류값 겹쳐질 때
+    //동일한 키값을 받아서 덮어쓸 때,
     void  testGet4() {
         //given
         Hash hash = new Hash(10);
@@ -143,8 +123,6 @@ class HashTest {
 
 
     }
-
-
 
 
 }
